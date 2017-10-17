d650f57ddaf7223d46a800e41a35686c91905ec5
gavin103 <541456119@qq.com>
2017-10-17 08:55:22 +0800

    title

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import Header from "../../components/Header/index";
 export default class Home extends Component{
     render(){
         return(
             <div>
-                Home
+                <Header icon="iconfont icon-iconfontmoban" title="数码广场"/>
+                <div className="container">
+
+                </div>
             </div>
         )
     }
